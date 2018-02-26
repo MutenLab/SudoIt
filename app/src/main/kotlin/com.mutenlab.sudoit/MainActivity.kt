a0f6ffb0584c988fa0af49b71eec3edbfe1c7b98
Ivan Cerrate <ivan.ccerrate@gmail.com>
2018-02-25 21:24:40 -0500

    Working on OCR implementation

--- app/src/main/kotlin/com.mutenlab.sudoit/MainActivity.kt
+++ app/src/main/kotlin/com.mutenlab.sudoit/MainActivity.kt
@@ -1,12 +1,20 @@
 package com.mutenlab.sudoit
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var btnStartOcr = findViewById<Button>(R.id.start_ocr)
+        btnStartOcr.setOnClickListener {
+            val intent = Intent(applicationContext, OcrCaptureActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
---
+++ app/src/main/kotlin/com.mutenlab.sudoit/ui/OcrDetectorProcessor.kt
@@ -0,0 +1,31 @@
+package com.mutenlab.sudoit.ui
+
+import android.util.Log
+import com.google.android.gms.vision.Detector
+import com.google.android.gms.vision.text.TextBlock
+import com.mutenlab.sudoit.ui.camera.GraphicOverlay
+
+/**
+ * A very simple Processor which gets detected TextBlocks and adds them to the overlay
+ * as OcrGraphics.
+ * TODO: Make this implement Detector.Processor<TextBlock> and add text to the GraphicOverlay
+</TextBlock> */
+class OcrDetectorProcessor internal constructor(private val mGraphicOverlay: GraphicOverlay<OcrGraphic>) : Detector.Processor<TextBlock> {
+    override fun receiveDetections(detections: Detector.Detections<TextBlock>) {
+        mGraphicOverlay.clear()
+        val items = detections.detectedItems
+        for (i in 0..items.size() - 1) {
+            val item = items.valueAt(i)
+            if (item != null && item.value != null) {
+                Log.d("Processor", "Text detected! " + item.value)
+            }
+            val graphic = OcrGraphic(mGraphicOverlay, item)
+            mGraphicOverlay.add(graphic)
+        }
+    }
+
+    override fun release() {
+        mGraphicOverlay.clear()
+    }
+    // TODO:  Once this implements Detector.Processor<TextBlock>, implement the abstract methods.
+}
